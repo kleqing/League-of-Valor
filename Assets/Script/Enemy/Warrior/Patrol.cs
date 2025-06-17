7f6c824d9dd284dd706ac96527b1500bc449047c
kleqing <nhanpk28122004@gmail.com>
2025-06-17 13:57:14 +0700

    [Add] Add patrolling for ground enemy

---
+++ Assets/Script/Enemy/Warrior/Patrol.cs
@@ -0,0 +1,82 @@
+using System;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    [Header("Patrol Settings")] 
+    [SerializeField] private Transform pointA;
+    [SerializeField] private Transform pointB;
+
+    [Header("Enemy Settings")]
+    [SerializeField] private Transform enemy;
+
+    [Header("Movement")]
+    [SerializeField] private float speed;
+    
+    [Header("Idle Behavior")]
+    [SerializeField] private float idleDuration;
+    
+    private Vector3 initialScale;
+    private bool isMoveLeft;
+    private float idleTimer;
+    
+    private Animator animator;
+
+    private void Awake()
+    {
+        initialScale = enemy.localScale;
+        animator = enemy.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (isMoveLeft)
+        {
+            if (enemy.position.x >= pointA.position.x)
+            {
+                MoveInDirection(-1f);
+            }
+            else
+            {
+                ChangeDirection();
+            }
+        }
+        else
+        {
+            if (enemy.position.x <= pointB.position.x)
+            {
+                MoveInDirection(1f);
+            }
+            else
+            {
+                ChangeDirection();
+            }
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        animator.SetBool("Run", false);
+        idleTimer += Time.deltaTime;
+        
+        if (idleTimer >= idleDuration)
+        {
+            isMoveLeft = !isMoveLeft;
+        }
+    }
+
+    private void MoveInDirection(float direction)
+    {
+        idleTimer = 0;
+        animator.SetBool("Run", true);
+        enemy.localScale = new Vector3(Mathf.Abs(initialScale.x) * direction, initialScale.y, initialScale.z);
+        
+        //* Move enemy between point A and point B
+        enemy.position = new Vector3(enemy.position.x + Time.deltaTime * direction * speed, enemy.position.y, enemy.position.z);
+    }
+
+    private void OnDisable()
+    {
+        animator.SetBool("Run", false);
+    }
+}
