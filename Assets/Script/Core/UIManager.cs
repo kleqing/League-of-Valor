6598a7f476087e4fbc4a7fe0f66417d6be95a1a3
kleqing <nhanpk28122004@gmail.com>
2025-06-19 16:32:39 +0700

    [Update] Add main menu, fix warning

--- Assets/Script/Core/UIManager.cs
+++ Assets/Script/Core/UIManager.cs
@@ -1,48 +1,78 @@
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [Header("UI Elements")] 
     [SerializeField] private GameObject pauseMenu;
     
-    private PlayerInput playerInput;
-    private InputAction menu;
+    [Header("Revive Settings")]
+    [SerializeField] private GameObject gameOverMenu;
+    [SerializeField] private Text remainingReviveText;
+    [SerializeField] private int maxRevives;
     
-    private bool isPaused;
+    [Header("Menu")]
+    [SerializeField] private GameObject mainMenu;
     
-    public bool IsPaused => isPaused;
+    private PlayerInput _playerInput;
+    private InputAction _menu;
+    private HealthBar _healthBar;
+    
+    private bool _isPaused;
+    private int _currentRevives;
+    
+    public bool IsPaused => _isPaused;
     
     private void Awake()
     {
-        playerInput = new PlayerInput();
+        _playerInput = new PlayerInput();
         if (pauseMenu != null)
         {
             pauseMenu.SetActive(false);
         }
+        
+        if (gameOverMenu != null)
+        {
+            gameOverMenu.SetActive(false);
+        }
+
+        _currentRevives = maxRevives;
+        
+        var player = FindFirstObjectByType<Player>();
+        if (player != null)
+        {
+            _healthBar = player.GetComponent<HealthBar>();
+        }
+        
+        if (mainMenu != null)
+        {
+            mainMenu.SetActive(true);
+        }
+        
+        UpdateReviveText();
     }
 
     private void OnEnable()
     {
-        menu = playerInput.Menu.UI;
-        menu.Enable();
-        menu.performed += Pause;
-        playerInput.Player.Disable();
+        _menu = _playerInput.Menu.UI;
+        _menu.Enable();
+        _menu.performed += Pause;
+        _playerInput.Player.Disable();
     }
 
     private void OnDisable()
     {
-        menu.Disable();
-        menu.performed -= Pause;
-        playerInput.Player.Disable();
+        _menu.Disable();
+        _menu.performed -= Pause;
+        _playerInput.Player.Disable();
     }
 
     public void Pause(InputAction.CallbackContext context)
     {
-        isPaused = !isPaused;
-        if (isPaused)
+        _isPaused = !_isPaused;
+        if (_isPaused)
         {
             ActivateMenu();
         }
@@ -56,19 +86,19 @@ public class UIManager : MonoBehaviour
     {
         Time.timeScale = 0;
         pauseMenu.SetActive(true);
-        playerInput.Player.Disable();
-        playerInput.Menu.Enable();
-        playerInput.Menu.UI.Disable();
+        _playerInput.Player.Disable();
+        _playerInput.Menu.Enable();
+        _playerInput.Menu.UI.Disable();
     }
     
     public void DeactivateMenu()
     {
         Time.timeScale = 1;
         pauseMenu.SetActive(false);
-        isPaused = false;
-        playerInput.Player.Disable();
-        playerInput.Menu.Disable(); 
-        playerInput.Menu.UI.Enable();
+        _isPaused = false;
+        _playerInput.Player.Disable();
+        _playerInput.Menu.Disable(); 
+        _playerInput.Menu.UI.Enable();
     }
 
     public void Quit()
@@ -85,5 +115,56 @@ public class UIManager : MonoBehaviour
     {
         Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GameOver()
+    {
+        if (gameOverMenu != null)
+        {
+            gameOverMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void Revive()
+    {
+        if (_healthBar == null)
+        {
+            return;
+        }
+        if (_currentRevives > 0)
+        {
+            _currentRevives--;
+            _healthBar.Revive();
+            UpdateReviveText();
+            
+            if (gameOverMenu != null)
+            {
+                gameOverMenu.SetActive(false);
+            }
+
+            Time.timeScale = 1f;
+
+            _playerInput.Player.Enable();
+            _playerInput.Menu.Disable();
+            _playerInput.Menu.UI.Enable();
+        }
+        else
+        {
+            gameOverMenu.SetActive(true);
+        }
+    }
+    
+    public void UpdateReviveText()
+    {
+        if (remainingReviveText != null)
+        {
+            remainingReviveText.text = $"{_currentRevives} remaining";
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
     }
 }
